cd8e678fa8c786c4575af4bc51091ce06e42e4a4
USU-Nervous <safeday@mail.ru>
2011-01-19 08:10:28 +0500

    fixed 1st

--- 1/1.c
+++ 1/1.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
+#include<errno.h>
 int main()
 {
-	printf("Hello, OS!!!/n");
-	return 0;
+	int err = 0;
+	printf("Hello, OS!!!\n");
+	err = errno;
+	if (err) 
+		{
+			fprintf(stderr,"Hello, OS!!!\n");
+		}
+	return err;
 }
