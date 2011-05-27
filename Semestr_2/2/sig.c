3a186de3935fec18273e8ec057431e59ec205523
USU-Nervous <safeday@mail.ru>
2011-05-27 14:54:56 +0600

    signals added

---
+++ Semestr_2/2/sig.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+
+const int EXIT_SIGNAL = 30;
+int pipefd[2];
+
+void sigHandler(int number, siginfo_t *info, void *something)
+{
+	int _pid = getpid();
+	int _gid = getpgrp();
+	write(pipefd[1], &number, sizeof(number));
+	write(pipefd[1], &_pid, sizeof(number));
+	write(pipefd[1], &_gid, sizeof(number));
+	if(number==EXIT_SIGNAL)
+		 close(pipefd[1]);
+}
+
+int main()
+{
+	struct sigaction S;
+	int number,_pid,_gid;
+
+	if (pipe(pipefd) == -1) 
+	{
+		fprintf(stderr,"Failed to create a pipe");
+        return 1;
+    }
+
+	S.sa_sigaction = sigHandler;
+	S.sa_flags = SA_SIGINFO;
+	if(sigfillset(&S.sa_mask))
+	{
+		fprintf(stderr,"Error:Signal mask is not set");
+		return 2;
+	}
+
+	for(int i=1; i<32; ++i)
+		if(i!=9 && i!=19 && sigaction(i, &S, NULL))
+		{
+			fprintf(stderr,"Error:Can't assing a handler");
+			return 3;
+		}
+
+	do
+	{
+		if(read(pipefd[0], &number, sizeof(number)) > 0)
+			{
+			read(pipefd[0], &_pid, sizeof(number));
+			read(pipefd[0], &_gid, sizeof(number));
+			fprintf(stderr,"Signal %d arrived pid=%d, gid=%d\n", number, _pid, _gid);
+			}
+	}
+	while(number != EXIT_SIGNAL);
+	return 0;
+}
